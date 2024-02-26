0b43b6d2c2f00d96e2040fae8e7f1d7e7880a1f2
1matrix9 <amralkhatib14@gmail.com>
2024-02-26 11:10:09 +0300

    first commit

---
+++ scripts/projects.js
@@ -0,0 +1,17 @@
+var PROJECTS = [
+  {
+    img: "project1.jpg",
+    title: "مشروع1",
+    description: "نبذة عن مشروع 1",
+  },
+  {
+    img: "project2.jpg",
+    title: "مشروع 2",
+    description: "نبذة عن مشروع 2",
+  },
+  {
+    img: "project1.jpg",
+    title: "مشروع3",
+    description: "نبذة عن مشروع 3",
+  },
+];
